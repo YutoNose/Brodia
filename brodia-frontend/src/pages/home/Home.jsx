9168115aeb0189df5cf839d5b3b2af455b7ad09d
YutoNose <yutoria0916@gmail.com>
2023-09-02 21:01:39 +0900

    大幅更新

--- brodia-frontend/src/pages/home/Home.jsx
+++ brodia-frontend/src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Topbar from '../../components/topbar/Topbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import TimeLine from '../../components/timeline/TimeLine'
+import Rightbar from '../../components/rightbar/Rightbar'
+import "./Home.css"
+
+// homeContainerで囲むことでflexによる横並びが可能
+// width 100% とかの意味を改めて把握しときたい
 
 export default function home() {
   return (
     <>
         <Topbar />
-
+        <div className="homeContainer">
+          <Sidebar />
+          <TimeLine />
+          <Rightbar/>
+        </div>
     </>
   )
 }
